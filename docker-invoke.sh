0fd8b48a3595874a5bdca186cfa91816b77150a3
claytonj222 <42755804+claytonj222@users.noreply.github.com>
2025-03-07 16:46:07 -0500

    Update docker-invoke.sh

--- docker-invoke.sh
+++ docker-invoke.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 
+# Verify a path was provided
+[ -n "$__path" ] || die "path is required"
+# Verify the path exists
+[ -f "$__path" ] || die "path ($__path) is not a file"
+
+docker run comskip-docker ./post-process.sh $__path
+
